af5899772e12c95d1e1881dbf97a38feb58f1508
cherzs <zhafrang638@gmail.com>
2024-11-05 16:28:32 +0700

    Separate Pos Margin Sale module

--- pos_margin_sale/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import product
-from . import pos_session
-from . import sale_order
-from . import res_config_settings
-from . import pos_config
--- pos_margin_sale/wizard/__init__.py
+++
@@ -1,2 +0,0 @@
-from . import sale_confirmation
-from . import wizard_margin_product
