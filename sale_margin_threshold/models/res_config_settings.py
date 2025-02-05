ca1d8edf333b5ee938559946660fbf1e04d35054
cherzs <zhafrang638@gmail.com>
2025-02-05 14:54:41 +0700

    Delete unnecesarry

--- sale_margin_threshold/models/res_config_settings.py
+++ sale_margin_threshold/models/res_config_settings.py
@@ -5,5 +5,4 @@ from odoo import _, api, fields, models
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    pos_epson_printer_ip = fields.Char("POS Epson Printer IP", config_parameter="pos_margin_threshold.pos_epson_printer_ip")
     blocking_transaction_order = fields.Boolean("Blocking Transaction Order", config_parameter="post_margin_sale.blocking_transaction_order")
